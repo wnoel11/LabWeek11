87bf5dd764d76b71c6f3b8b188fba5c577f6aed4
wnoel11 <43223925+wnoel11@users.noreply.github.com>
2018-11-28 18:57:49 -0500

    Add project files.

---
+++ LabWeek11/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabWeek11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //#1
+
+            string[] names = { "Al Dente", "Anna Graham", "Earle Bird", "Ginger Rayle", "Iona Ford" };
+
+            Console.WriteLine(names[2]);
+            Console.WriteLine(names[4]);
+
+            Console.ReadLine();
+
+            //#2
+
+              string[] month = new string[12];
+                month[0] = "January";
+                month[1] = "February";
+                month[2] = "March";
+                month[3] = "April";
+                month[4] = "May";
+                month[5] = "June";
+                month[6] = "July";
+                month[7] = "August";
+                month[8] = "September";
+                month[9] = "October";
+                month[10] = "November";
+                month[11] = "December";
+           
+         
+                    for (int i = 0; i < month.Length; i++)
+                    {
+                    Console.WriteLine(i.ToString() + month[i]);
+                    }
+                
+             
+            Console.ReadLine();
+                                
+
+            //#3
+
+            string[] seasons = {"Spring", "Summer", "Fall", "Winter"};
+            int y = 0;
+
+            for (y = 0; y < seasons.Length; y++)
+            {
+                Console.WriteLine(seasons[y]);
+            }
+
+            Console.ReadLine();
+
+            //#4
+
+            int[] randomNumber = new int[1000];
+            Random random = new Random();
+
+            for (int z = 0; z < randomNumber.Length; z++)
+            {
+                randomNumber[z] = random.Next(0, 100);
+            }
+
+            foreach (int z in randomNumber)
+            {
+                Console.WriteLine(z.ToString());
+            }
+        }
+    }
+}
---
+++ LabWeek11/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LabWeek11")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LabWeek11")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("50530128-9141-4793-9257-e62cac90ba8c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
